c05bbe7f202539e0f8c94531403a27c593f46fb4
Anibal1234 <anibalrodrigues2001@gmail.com>
2023-04-06 19:52:11 +0100

    update
    Temos os processos workers e shared memory

--- SystemManager.c
+++ SystemManager.c
@@ -1,3 +1,6 @@
+//gcc -Wall -pthread SystemManager.c -o run
+
+
 
 #include <stdio.h>
 #include <time.h>
@@ -14,6 +17,79 @@
 #include <sys/wait.h>
 #include <pthread.h>
 
+#include "structs.h"
+
+#define bufferLength 255
+
+//global variables
+int shmid;
+FILE* conf;
+char buffer[bufferLength];
+int queue_size;
+int n_workers;
+int max_keys;
+int max_sensors;
+int max_alerts;
+int conf_counter = 0;
+pid_t pid;
+
+void confAttribution(){
+  conf = fopen("configFile.txt","r");
+  if(conf == NULL){
+    printf("Config file can't be opened\n");
+    exit(0);
+  }
+  while(fgets(buffer,bufferLength, conf)){
+    printf("%s\n", buffer);
+    conf_counter ++;
+    if(conf_counter == 1){
+        queue_size = atoi(buffer);
+    }else if(conf_counter == 2){
+        n_workers = atoi(buffer);
+    }else if(conf_counter == 3){
+        max_keys = atoi(buffer);
+    }else if(conf_counter == 4){
+        max_sensors = atoi(buffer);
+    }else if(conf_counter == 5){
+        max_alerts = atoi(buffer);
+    }else{
+      printf("Config file is wrong!! \n");
+      exit(0);
+    }
+  }
+  fclose(conf);
+  //printf("SAO ESTAS : %d, %d, %d, %d, %d \n",queue_size,n_workers,max_keys,max_sensors,max_alerts);
+}
+
+void sharedmem(){
+  shmid = shmget(IPC_PRIVATE,sizeof(shm_t),IPC_CREAT|0777);
+  if(shmid == -1){
+		perror("FAILED TO CREATE SHARED MEMORY!");
+    exit(0);
+	}
+
+  shm = (shm_t*) shmat(shmid,NULL,0);
+
+  if(shm == NULL){
+    perror("FAILED ATTACHING SHARED MEMORY!");
+    exit(0);
+  }
+
+  printf("SHARED MEMORY CREATED AND ATTACHED!\n");
+
+}
+
+void end_it_all(){
+  shmdt(shm);
+  shmctl(shmid,IPC_RMID, NULL);
+
+}
+
+void waitforworkers() {
+    for (int i = 0; i < n_workers; i++) {
+      wait(NULL);
+    }
+}
 
 
 int main(int argc, char** argv) {
@@ -23,6 +99,23 @@ int main(int argc, char** argv) {
       printf("This is the user console!!! \n");
     }else if(strcmp(argv[1],"home_iot") == 0){
       printf("This is the system Manager!!! \n");
+      confAttribution();
+      sharedmem();
+
+      for(int i = 0; i<n_workers;i++){
+        pid = fork();
+        if(pid == 0){
+          printf("%d : I'm a child/worker process with a pid of %d and my dad is %d\n", i+1,getpid(), getppid());
+          break;
+        }else if(pid<0){
+          perror("Error creating child/worker processes");
+          exit(0);
+        }else{
+          printf("I'M THE BIG PAPA!!!\n");
+        }
+      }
+
+
     }else{
       printf("Opções invalidas!!\n");
       exit(0);
@@ -39,5 +132,6 @@ int main(int argc, char** argv) {
     printf("Opçoes invalidas!\n");
   }
 
-
+waitforworkers();
+end_it_all();
 }
---
+++ structs.h
@@ -0,0 +1,14 @@
+#ifndef UNTITLED1_STRUCTS_H
+#define UNTITLED1_STRUCTS_H
+
+#include <stdbool.h>
+
+
+typedef struct Shared {
+    int treta1;
+    int treta2;
+} shm_t;
+
+shm_t* shm;
+
+#endif //UNTITLED1_STRUCTS_H
